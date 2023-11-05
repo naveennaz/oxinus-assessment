642c749d65c58915681e2b000130645bb5f55778
Naveen <naveeen@aaicenter.ae>
2023-11-05 23:55:21 +0400

    username and password changed

--- library/email.js
+++ library/email.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'naveeine@gmail.com', // Replace with your email address
-    pass: '$naveen@123#' // Replace with your email password (or use environment variables)
+    user: '', // Replace with your email address
+    pass: '' // Replace with your email password (or use environment variables)
   }
 });
 
