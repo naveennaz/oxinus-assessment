417b39657c799d2926fd44698767970bc4ec3ce6
naveennaz <naveen.nn2@gmail.com>
2025-06-04 12:47:07 +0400

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./oxinus-test-firebase-adminsdk-v13we-ea7274cc6b.json'); // Replace with the path to your service account key JSON file
+const serviceAccount = require(''); // Replace with the path to your service account key JSON file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://oxinus-test-default-rtdb.firebaseio.com', // Replace with your Firebase project's database URL
+  databaseURL: '', // Replace with your Firebase project's database URL
 });
 
 
